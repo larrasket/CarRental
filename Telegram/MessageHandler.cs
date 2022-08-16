5121298b11432e99d3c107b369c5c0ef1ef3f628
salehmu <ghd@keemail.me>
2022-08-16 22:04:01 +0200

    rent implementation

--- Telegram/MessageHandler.cs
+++ Telegram/MessageHandler.cs
@@ -1,38 +1,81 @@
-using Models.DataModels;
 using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
 using Telegram.BotAPI.GettingUpdates;
+using Telegram.Languages;
 
 namespace Telegram;
 
 public class MessageHandler
 {
     private readonly BotClient _client;
-    private readonly Update? _update;
+    private Update? _update;
     private readonly OpenMessages _openMessages;
+    private readonly Commander _commander;
 
     public MessageHandler(BotClient client, Update? update)
     {
         _client = client;
         _update = update;
+        _commander = new Commander(_client);
         _openMessages = new OpenMessages(_client);
     }
 
-    public async Task HandleOpen()
+    public async Task MainOpen(bool start = false)
     {
-        switch (await _update.Text())
+        // if (!start)
+        _update = await _openMessages.Usage(_update);
+        var f = true;
+        while (f)
         {
-            case "/admin":
-                await Admin(await _openMessages.Admin(_update));
-                break;
+            f = false;
+            switch (_update.Text())
+            {
+                case "/admin":
+                    await Admin(await _openMessages.Admin(_update));
+                    break;
+                case "/addrent":
+                    await _commander.AddRent(_update);
+                    break;
+                default:
+                    f = true;
+                    await _client.SendMessageAsync(_update.ChatId(), Arabic.EntreValidOption);
+                    await _client.SendMessageAsync(_update.ChatId(), Arabic.Usage);
+                    _update = await _client.MessageWatcher(_update);
+                    break;
+            }
         }
     }
 
-    private async Task Admin(Update? action)
+
+    private async Task Admin(Update? update)
     {
-        switch (await _update.Text())
+        var f = true;
+        while (f)
         {
-            case "/add":
-                Vehicle vehicle = DataReader.Vehicle(_update);
+            f = false;
+            switch (update.Text())
+            {
+                case "/add":
+                    await _commander.AddVehicle(update ?? throw new ArgumentNullException(nameof(update)));
+                    await _client.SendMessageAsync(update.ChatId(), Arabic.CarAdded);
+                    break;
+                case "/list":
+                    await _commander.ListVehicles(update);
+                    break;
+                case "/remove":
+                    await _commander.RemoveVehicle(update);
+                    await _client.SendMessageAsync(update.ChatId(), Arabic.CarDeleted);
+                    break;
+                default:
+                    f = true;
+                    await _client.SendMessageAsync(_update.ChatId(), Arabic.EntreValidOption);
+                    update = await _openMessages.Admin(_update);
+                    break;
+            }
         }
+
+// TODO Call Main 
+        _update = await _client.MessageWatcher();
+        await MainOpen();
     }
 }
