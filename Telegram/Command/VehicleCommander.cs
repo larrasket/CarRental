e6fd1ccd6041d2eaadef3ab0d2cc799e78c9cdcf
salehmu <ghd@keemail.me>
2022-08-19 09:42:01 +0200

    inti new file structure

--- Telegram/Command/VehicleCommander.cs
+++ Telegram/Command/VehicleCommander.cs
@@ -1,6 +1,95 @@
 namespace Telegram.Command;
 
-public class VehicleCommander
+using System.Text;
+using Models.DataModels;
+using Models.Helpers;
+using Services;
+using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.GettingUpdates;
+using Telegram.Languages;
+
+public partial class Commander
 {
-    
+    private readonly TypeManager<Vehicle> _vehicleManager = new TypeManager<Vehicle>();
+
+    public async Task<Update> AddVehicle(Update update)
+    {
+        var vehicle = new Vehicle();
+
+        update = await GenericOptions(update, _vehicleManager.All().Select(x => x.Brand), Arabic.AddBrand);
+        vehicle.Brand = update.Text();
+
+        update = await GenericOptions(update, _vehicleManager.All().Select(x => x.Model), Arabic.AddModel);
+        vehicle.Model = update.Text();
+
+        update = await GenericOptions(update, _vehicleManager.All().Select(x => x.Color), Arabic.AddColor);
+        vehicle.Color = update.Text();
+
+        var numbers = _vehicleManager.All().Select(x => x.Number);
+        while (true)
+        {
+            update = await GenericOptions(update, _vehicleManager.All().Select(x => x.Number), Arabic.AddNumber);
+            if (numbers.Any(x => x == update.Text()) && update.Text() != "/cancel")
+            {
+                await _client.SendMessageAsync(update.ChatId(), Arabic.CarNumberAlreadyExist);
+            }
+            else if (update.Text() == "/cancel")
+            {
+                return update;
+            }
+            else break;
+        }
+
+        vehicle.Number = update.Text();
+        vehicle.CreatedBy = update.UserName();
+        var rs = await _vehicleManager.Add(vehicle);
+        if (rs != 1) throw new Exception();
+        return update;
+    }
+
+
+    public Task ListVehicles(Update? update, bool admin = false, bool allowContracts = false)
+    {
+        var vehicles = _vehicleManager.All();
+        var rents = _rentManager.All();
+        var message = new StringBuilder();
+        foreach (var vehicle in vehicles)
+        {
+            if (admin) message.Append($"{Arabic.CarDetails.Number}: /{vehicle.Number}");
+            else message.Append($"{Arabic.CarDetails.Number}: {vehicle.Number}");
+
+            message.Append("\n");
+
+
+            message.Append($"{Arabic.CarDetails.Model}: {vehicle.Model}");
+            message.Append("\n");
+
+
+            message.Append($"{Arabic.CarDetails.Brand}: {vehicle.Brand}");
+            message.Append("\n");
+
+
+            message.Append($"{Arabic.CarDetails.Color}: {vehicle.Color}");
+            message.Append("\n");
+
+            var now = DateOnly.FromDateTime(DateTime.Now);
+            var contracts = rents.Where(x => x.VehicleId == vehicle.Id && x.RentEnd > now);
+            if (!contracts.Any() || !allowContracts) continue;
+            foreach (var contract in contracts)
+            {
+                message.Append($"{Arabic.CarDetails.Contract}: /cont{contract.Id}");
+                message.Append("\n");
+            }
+        }
+
+        _client.SendMessageAsync(update.ChatId(), message.ToString().PadRight(3));
+        return Task.CompletedTask;
+    }
+
+    public async Task RemoveVehicle(Update? update)
+    {
+        var v = await ChooseVehicle(update, true);
+        await _vehicleManager.Remove(v.v);
+    }
 }
--- Telegram/Command/VehicleHelper.cs
+++ Telegram/Command/VehicleHelper.cs
@@ -1,6 +1,30 @@
+using Models.DataModels;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.GettingUpdates;
+using Telegram.Languages;
+
 namespace Telegram.Command;
 
-public class VehicleHelper
+public partial class Commander
 {
-    
+    private async Task<(Vehicle v, Update update)> ChooseVehicle(Update update, bool admin = false,
+        bool contracts = false)
+    {
+        await ListVehicles(update, admin, contracts);
+        await _client.SendMessageAsync(update.ChatId(), Arabic.CarDetails.EnterNumber);
+        update = await _client.MessageWatcher(update);
+
+        var selected = update.Text()[1..];
+        var v = _vehicleManager.All().FirstOrDefault(x => x.Number == selected);
+        if (contracts) return (v, update);
+        {
+            while (v is null)
+            {
+                update = await _client.MessageWatcher(update);
+                v = _vehicleManager.All().FirstOrDefault(x => x.Number == selected);
+            }
+        }
+
+        return (v, update);
+    }
 }
