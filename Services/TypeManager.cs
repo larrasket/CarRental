8725f571611d55ed23fd5ae3014653978e377f10
salehmu <ghd@keemail.me>
2022-08-19 09:42:12 +0200

    special type for rents
    gets only available ones

--- Services/TypeManager.cs
+++ Services/TypeManager.cs
@@ -5,28 +5,39 @@ namespace Services;
 
 public class TypeManager<T> : IManager<T> where T : BaseModel
 {
-    private readonly CycleContext _db;
-    public TypeManager() => _db = new CycleContext();
+    public readonly CycleContext Db;
+    public TypeManager() => Db = new CycleContext();
 
-    public IQueryable<T> All() => _db.Set<T>().AsQueryable();
+    public IQueryable<T> All()
+    {
+        return Db.Set<T>().AsQueryable();
+    }
+
+
 
     public async Task<int> Add(T i)
     {
-        await _db.Set<T>().AddAsync(i);
-        return await _db.SaveChangesAsync();
+        await Db.Set<T>().AddAsync(i);
+        return await Db.SaveChangesAsync();
     }
 
     public async Task<int> Edit(T i)
     {
 #pragma warning disable CS8604
-        _db.Entry<T>(await _db.Set<T>().FindAsync(i)).CurrentValues.SetValues(i);
+        Db.Entry<T>(await Db.Set<T>().FindAsync(i.Id)).CurrentValues.SetValues(i);
 #pragma warning restore CS8604
-        return await _db.SaveChangesAsync();
+        return await Db.SaveChangesAsync();
+    }
+
+
+    public async Task<T?> Find(long i)
+    {
+        return await Db.Set<T>().FindAsync(i);
     }
 
     public Task<int> Remove(T i)
     {
-        _db.Set<T>().Remove(i);
-        return _db.SaveChangesAsync();
+        Db.Set<T>().Remove(i);
+        return Db.SaveChangesAsync();
     }
 }
