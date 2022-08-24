363ac7f59f02c277032a4d05d8ce29e419f3b049
salehmu <ghd@keemail.me>
2022-08-24 03:22:45 +0200

    migration files

---
+++ Services/Migrations/20220824005305_RentDriver.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Services.Migrations
+{
+    public partial class RentDriver : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Driver",
+                table: "Rents",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Driver",
+                table: "Rents");
+        }
+    }
+}
